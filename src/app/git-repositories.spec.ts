ac39f149a1a99f3ca90012de4fe12d979f8e29d2
emile kamana <emileizere@gmail.com>
2020-11-14 22:47:44 +0200

    create users and repositories classes

---
+++ src/app/git-repositories.spec.ts
@@ -0,0 +1,7 @@
+import { GitRepositories } from './git-repositories';
+
+describe('GitRepositories', () => {
+  it('should create an instance', () => {
+    expect(new GitRepositories()).toBeTruthy();
+  });
+});
