21003ae3f99f703facc5b192da3c47518e176d9b
emile kamana <emileizere@gmail.com>
2020-11-15 23:20:52 +0200

    edit date-count pipe function

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,13 +5,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    var dateToday: any = new Date();
-    var diffMs = (dateToday - value);
-    var diffDays = Math.floor(diffMs / 86400000); // days
-    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
-    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
-    return diffDays;
-  }
+  transform(value: any, args?: any): any {
+    if (value) {
+        const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+        if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
+            return 'Just now';
+        const intervals = {
+            'year': 31536000,
+            'month': 2592000,
+            'week': 604800,
+            'day': 86400,
+            'hour': 3600,
+            'minute': 60,
+            'second': 1
+        };
+        let counter;
+        for (const i in intervals) {
+            counter = Math.floor(seconds / intervals[i]);
+            if (counter > 0)
+                if (counter === 1) {
+                    return counter + ' ' + i + ' ago'; // singular (1 day ago)
+                } else {
+                    return counter + ' ' + i + 's ago'; // plural (2 days ago)
+                }
+        }
+    }
+    return value;
+}
 
 }
