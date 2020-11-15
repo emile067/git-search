65dc5ec0c0e36e16cbf05f52e66b4aeb3b1981ea
emile kamana <emileizere@gmail.com>
2020-11-15 22:21:14 +0200

    create subscriptions for home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,20 @@ import { GitServiceService } from '../git-service.service';
 })
 export class HomeComponent implements OnInit {
 
+  homeUser: any;
+  homeRepos: any;
+  constructor(private userService: GitServiceService) {
+  }
 
   ngOnInit(): void {
+    this.userService.getUserInfo('emile067').subscribe(users => {
+      this.homeUser = users;
+      console.log(JSON.stringify(this.homeUser));
+    });
+    this.userService.getUserRepos('emile067').subscribe(repos => {
+      this.homeRepos = repos;
+      console.log(JSON.stringify(this.homeRepos));
+    });
   }
 
 }
